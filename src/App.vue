8719c84f3ce568f4e6453c4ab828641060c3ada8
Joseph Steel <joseph.steel.iv@gmail.com>
2022-05-20 08:56:30 -0400

    Update App.vue
    dragging functional

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div class='container mt-5'>
     <div class="row">
-      <div 
-      class="col-6 col-sm-3 col-lg-2" 
-      v-for="(item, index) in IconList" 
-      :class="item.class" :key="item.id" 
-      :id="`drop-zone-${index}`" 
+      <div
+      class="col-6 col-sm-3 col-lg-2 bg-light m-3"
+      v-for="(item, index) in IconList"
+      :class="item.class" :key="item.id"
+      :id="`drop-zone-${index}`"
       @drop="drop($event, index)"
       @dragover.prevent
       @dragenter.prevent>
-        <div 
-        class="drop-zone bg-light p-3 mt-4" 
+        <div
+        class="p-3"
         :draggable="item.isDraggable"
-        @mousedown="mouseDown(index)" 
-        @mouseup="clearTimeDrag(index)" 
+        @mousedown="mouseDown(index)"
+        @mouseup="clearTimeDrag(index)"
         @mouseleave="clearTimeDrag(index)"
-        @dragstart="dragStart($event, item.id)">
-          <div class="d-flex justify-content-center flex-wrap">
-            <img src="./assets/logo.png" height="80" width="80" draggable="false"/>
+        @dragstart="dragStart($event, item.position)">
+          <div class="row">
+            <img class="ratio ratio-1x1" src="./assets/logo.png" draggable="false"/>
             <div class="text-primary align-self-center">{{item.label}}</div>
           </div>
         </div>
@@ -25,8 +25,10 @@
     </div>
   </div>
 </template>
+
 <script>
   import {defineComponent, onMounted, ref} from "vue";
+
   const App = defineComponent({
     name: "App",
     setup() {
@@ -35,66 +37,67 @@
       const initialIcons = [
         {
           id: 0,
-          label: 'Hello World 0',
+          label: 'Charizard',
           isDraggable: false,
           class: '',
+          position: 0,
         },
         {
           id: 1,
-          label: 'Hello World 1',
+          label: 'Blastoise',
           isDraggable: false,
-          class: '',          
+          class: '',
+          position: 1,
         },
         {
           id: 2,
-          label: 'Hello World 2',
+          label: 'Venosaur',
           isDraggable: false,
-          class: '',          
+          class: '',
+          position: 2,
         },
         {
           id: 3,
-          label: 'Hello World 3',
+          label: 'Pikachu',
           isDraggable: false,
-          class: '',          
+          class: '',
+          position: 3,
         },
         {
           id: 4,
-          label: 'Hello World 4',
+          label: 'Lapras',
           isDraggable: false,
-          class: '',          
+          class: '',
+          position: 4,
         },
         {
           id: 5,
-          label: 'Hello World 5',
+          label: 'Hitmonchan',
           isDraggable: false,
-          class: '',          
+          class: '',
+          position: 5,
         },
         {
           id: 6,
-          label: 'Hello World 6',
+          label: 'Zapdos',
           isDraggable: false,
-          class: '',          
+          class: '',
+          position: 6,
         },
       ];
-
       let IconList = ref([]);
       onMounted(() => {
         IconList.value = initialIcons;
       })
-
-
       // Logic for Draggability
       let mouseTimer;
-
       function setIsDraggable(index) {
         IconList.value[index].isDraggable = true;
-        IconList.value[index].class = 'bounce';
+        IconList.value[index].class = 'bounce-once';
       }
-
       function mouseDown(index) {
-        mouseTimer = window.setTimeout(function() {setIsDraggable(index)},1000);
+        mouseTimer = window.setTimeout(function() {setIsDraggable(index)},500);
       }
-
       function clearTimeDrag(index) {
         if (mouseTimer) {
           IconList.value[index].isDraggable = false;
@@ -102,28 +105,51 @@
           window.clearTimeout(mouseTimer);
         }
       }
-
-      function dragStart(e, id) {
-        console.log(e, id);
+      //dragging logic
+      function dragStart(e, position) {
         e.dataTransfer.dropEffect = 'move';
         e.dataTransfer.effectAllowed = 'move';
-        e.dataTransfer.setData('itemID', id);
+        e.dataTransfer.setData('itemPosition', position);
+        IconList.value[position].class = 'bounce';
       }
-
       function drop(e, index) {
-        const itemId = e.dataTransfer.getData('itemID');
-
-        let draggedItem = IconList.value[itemId];
+        const itemPosition = e.dataTransfer.getData('itemPosition');
+        IconList.value[itemPosition].class = '';
+        let draggedItem = IconList.value[itemPosition];
         draggedItem.class = '';
         draggedItem.isDraggable = false;
-
-        // const currentIndex = IconList.value.map(x => x.id).indexOf(itemId);
-        // console.log("current index: ", currentIndex, "drop-zone index: ", index);
-        IconList.value.splice(itemId, 1);
-        IconList.value.splice(index, 0, draggedItem);
-        console.log(draggedItem);
+        if(itemPosition > index) {
+          draggedForward(index, itemPosition);
+        } else if (itemPosition < index) {
+          draggedBackward(index, itemPosition);
+        }
+        sortIcons();
+        console.log(IconList.value);
       }
-
+      function draggedBackward(dropIndex, draggedIndex) {
+        for(let i=parseInt(draggedIndex); i <= dropIndex; i++) {
+          // console.log(IconList.value[0].position);
+          IconList.value[i].position = (IconList.value[i].position -1);
+        }
+        IconList.value[draggedIndex].position = dropIndex;
+      }
+      function draggedForward(dropIndex, draggedIndex) {
+        for(let i=dropIndex; i < draggedIndex; i++) {
+          IconList.value[i].position = IconList.value[i].position +1;
+        }
+        IconList.value[draggedIndex].position = dropIndex;
+      }
+      function sortIcons() {
+        IconList.value.sort(byPosition);
+      }
+      function byPosition(a, b) {
+        if (a.position < b.position) { return -1 }
+        else if (a.position > b.position) { return 1 }
+        return 0;
+      }
+      function dragOver(index) {
+        console.log(index);
+      }
       return {
         initialIcons,
         mouseDown,
@@ -131,6 +157,8 @@
         IconList,
         dragStart,
         drop,
+        sortIcons,
+        dragOver,
       }
     },
   })
@@ -140,6 +168,9 @@
 <style scoped>
 .bounce {
   animation: bounce 1.5s ease infinite;
+}
+.bounce-once {
+  animation: bounce 1.5s ease 1;
 }
 @keyframes bounce {
   15% { transform: scale(.9); }
